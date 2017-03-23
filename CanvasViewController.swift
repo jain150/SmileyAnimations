e491ba0f7985ec8aa8925d74ad15222853c7e4fe
Arnav Jain <jain150@purdue.edu>
2017-03-23 19:49:15 -0400

    Complete

---
+++ CanvasViewController.swift
@@ -0,0 +1,122 @@
+//
+//  CanvasViewController.swift
+//  AnimationsAndGestures
+//
+//  Created by Arnav Jain on 3/23/17.
+//  Copyright © 2017 Arnav Jain. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+    
+    @IBOutlet weak var trayView: UIView!
+    
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        trayDownOffset = 160
+        trayUp = trayView.center
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: view)
+        var velocity = sender.velocity(in: view)
+        
+        if sender.state == .began {
+            
+            trayOriginalCenter = trayView.center
+            
+        } else if sender.state == .changed {
+            
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            
+            
+        } else if sender.state == .ended {
+            
+            //velocity > 0 -> down
+            //else -> up
+            
+            if velocity.y > 0 {
+                UIView.animate(withDuration: 0.4, animations: {
+                    self.trayView.center = self.trayDown
+                })
+            } else {
+                UIView.animate(withDuration: 0.4, animations: {
+                    self.trayView.center = self.trayUp
+                })
+            }
+            
+        }
+        
+    }
+    
+    
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: view)
+        var imageView = sender.view as! UIImageView
+
+        
+        if sender.state == .began {
+            
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            view.addSubview(newlyCreatedFace)
+            newlyCreatedFace.center = imageView.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+            
+            newlyCreatedFace.isUserInteractionEnabled = true
+            
+            
+            //Animate
+            UIView.animate(withDuration: 0.2, animations: {
+                imageView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            })
+            
+            
+        } else if sender.state == .changed {
+            
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+            
+            
+        } else if sender.state == .ended {
+            
+            //Animate
+            UIView.animate(withDuration: 0.2, animations: {
+                imageView.transform = CGAffineTransform(scaleX: 1, y: 1)
+            })
+        }
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
