a6176c994f610072a1159135c66d5bdd910a706d
Arnav Jain <jain150@purdue.edu>
2017-03-23 18:31:49 -0400

    Initial Commit

---
+++ AnimationsAndGestures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationsAndGestures
+//
+//  Created by Arnav Jain on 3/23/17.
+//  Copyright © 2017 Arnav Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
